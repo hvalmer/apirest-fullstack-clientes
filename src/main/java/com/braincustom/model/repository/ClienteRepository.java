4791aef217c2660b5e65a4ee6a1acfa8c7762abd
hvalmer <hvalmer@gmail.com>
2023-09-02 15:44:41 -0300

    Criando os Repositorios JPA

---
+++ src/main/java/com/braincustom/model/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.braincustom.model.repository;
+
+import com.braincustom.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
---
+++ src/main/java/com/braincustom/model/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package com.braincustom.model.repository;
+
+import com.braincustom.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
