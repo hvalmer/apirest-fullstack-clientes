6c9c837b18936934fb6a605ed3222f6a971a4eee
hvalmer <hvalmer@gmail.com>
2023-09-19 22:51:17 -0300

    CRUD Cliente e config. Internacionalizacao

---
+++ src/main/java/com/braincustom/config/InternalicionalizacaoConfig.java
@@ -0,0 +1,32 @@
+package com.braincustom.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Locale;
+
+/*
+* Padronizando as classes de configuração
+**/
+@Configuration
+public class InternalicionalizacaoConfig {
+
+    @Bean //primeiro Bean vai ser a fonte das mensagens sera utilizada no sistema
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");//arquivo de mensagens(messages.properties)
+        messageSource.setDefaultEncoding("ISO-8859-1");//caracteres especiais
+        messageSource.setDefaultLocale(Locale.getDefault());//Locale do SO
+        return messageSource;
+    }
+
+    @Bean//outro Bean que faça a interpolacao
+    public LocalValidatorFactoryBean validatorFactoryBean(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
--- src/main/java/com/braincustom/rest/ClienteController.java
+++ src/main/java/com/braincustom/rest/ClienteController.java
@@ -5,6 +5,9 @@ import com.braincustom.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -19,7 +22,39 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)//201 criado
-    public Cliente salvar(@RequestBody Cliente cliente){
+    public Cliente salvar(@RequestBody @Valid Cliente cliente){
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente buscarPorId(@PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar( @PathVariable Integer id){
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    repository.delete(cliente);
+                    return  Void.TYPE;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado){
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    cliente.setNome(clienteAtualizado.getNome());
+                    cliente.setCpf(clienteAtualizado.getCpf());
+                    return repository.save(clienteAtualizado);
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
