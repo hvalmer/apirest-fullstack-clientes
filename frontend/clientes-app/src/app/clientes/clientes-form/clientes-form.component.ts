03c90e88a8dfe79a6d6134b626f1a7c7d63a40cc
hvalmer <hvalmer@gmail.com>
2024-01-12 11:18:02 -0300

    Desenvolvendo o formulário e populando o cliente

--- frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
   styleUrls: ['./clientes-form.component.css']
 })
-export class ClientesFormComponent {
+export class ClientesFormComponent implements OnInit {
 
+  cliente: Cliente;
+
+  constructor() {
+    this.cliente = new Cliente();
+  }
+  ngOnInit(): void{
+  }
+
+  clicar(){
+    console.log(this.cliente);
+  }
 }
